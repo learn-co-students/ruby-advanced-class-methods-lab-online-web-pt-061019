99912ae5d251f51eaba39375221afeee143b7de2
Jack Atherton <jackrrye@gmail.com>
2019-06-16 18:28:38 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,7 @@ class Song
   
   middle = Song.new_by_name("The Middle")
   secrets = Song.new_by_name("Secrets")
+  hello = Song.new_by_name("Hello")
   
   def self.create_by_name(song_name)
     song = self.create
@@ -34,16 +35,49 @@ class Song
   end
 
   def self.find_by_name(song_name)
-    song_match = Song.new 
+    song_match = nil 
     self.all.each do |song|
       if song_name == song.name
         song_match = song
-        #binding.pry
-      #else 
-        #song_match = false 
       end
     end
     song_match
   end
+  
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      #binding.pry
+      self.create_by_name(song_name)
+    else 
+      self.find_by_name(song_name)
+    end
+  end
+  
+  def self.alphabetical
+    #iterate through my song array
+    Song.all.sort_by do |song| 
+      song.name 
+    end 
+  end 
 
+  def self.new_from_filename(filename)
+    filename_array = filename.split(" - ")
+    filename_name_array = filename_array[1].split(".")
+    filename_song = Song.create_by_name(filename_name_array[0])
+    filename_song.artist_name = filename_array[0]
+    filename_song
+  end
+  
+  def self.create_from_filename(filename)
+    filename_array = filename.split(" - ")
+    filename_name_array = filename_array[1].split(".")
+    filename_song = Song.create_by_name(filename_name_array[0])
+    filename_song.artist_name = filename_array[0]
+    filename_song
+  end 
+  
+  def self.destroy_all 
+    @@all = []
+  end 
+  
 end
