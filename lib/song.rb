e4b836abd0f1ad3d09956232ec532faa53b01c91
Maasa Kono <konomaasa@gmail.com>
2019-07-06 21:51:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,12 +33,10 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.find_by_name(name) == true
-      @name
-    else
-      self.create_by_name(name)
-      @name
-    end
+    self.find_by_name(name)
+  else
+    self.create_by_name(name)
+  end
   end
 
 end
