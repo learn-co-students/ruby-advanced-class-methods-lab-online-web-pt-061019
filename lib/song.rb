580971da50e8cc7408089ffd9d42654f7e5b4651
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-11 14:21:51 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
     song
   end
   def self.create_from_filename(filename)
-    result = self.new_from_filename(filename)
+    output = self.new_from_filename(filename)
     song = self.create
     song.name = result.name
     song.artist_name = result.artist_name
