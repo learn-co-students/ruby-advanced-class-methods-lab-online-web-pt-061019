de55ad9e4fa6d2fbf17cb358015f9a39d8006c1c
Aida Abdikulova <abdikulova@gmail.com>
2019-07-16 04:04:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 class Song
-  attr_accessor :name, :artist_name
   @@all = []
+
+  attr_accessor :name, :artist_name
 
   def self.all
     @@all
@@ -10,4 +11,20 @@ class Song
     self.class.all << self
   end
 
+  def self.create()
+    new_song = self.new
+    @@all << new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+
+end
 end
